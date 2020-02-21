da1306dbbdf421ac01fa3b3c4f33a147af5fb174
Jessica Stenning <jessica.stenning@engineerbetter.com>
2020-02-21 11:38:24 +0000

    Add a ci directory for stress testing

---
+++ ci_stress_testing/tasks/system_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+
+export BOSH_NON_INTERACTIVE=true
+export BOSH_DEPLOYMENT=mongodb
+
+start-bosh \
+  -o /usr/local/bosh-deployment/uaa.yml \
+  -o /usr/local/bosh-deployment/credhub.yml
+source /tmp/local-bosh/director/env
+
+bosh upload-stemcell xenial_stemcell/*.tgz
+bosh upload-release mongodb/mongodb(.*).tgz
+
+bosh -d mongodb deploy mongodb_release/ci_stess_testing/files/manifest.yml
+
+bosh deploy mongodb_release/ci_stess_testing/files/manifest.yml \
+  --vars-store=/tmp/mongodb_vars.yml \
+
+# Submit example-topology via a Bosh errand
+bosh run-errand acceptance-tests
