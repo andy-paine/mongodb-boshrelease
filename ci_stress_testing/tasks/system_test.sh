31fdd924ee124578c3573613929f57e7b2bf1ca1
Andy Paine <andy.paine@engineerbetter.com>
2020-02-21 12:46:34 +0000

    Fix testing shell script

--- ci_stress_testing/tasks/system_test.sh
+++ ci_stress_testing/tasks/system_test.sh
@@ -11,11 +11,11 @@ start-bosh \
 source /tmp/local-bosh/director/env
 
 bosh upload-stemcell xenial_stemcell/*.tgz
-bosh upload-release mongodb/mongodb(.*).tgz
+bosh upload-release mongodb/mongodb*.tgz
 
-bosh -d mongodb deploy mongodb_release/ci_stess_testing/files/manifest.yml
+bosh -d mongodb deploy mongodb_release/ci_stress_testing/files/manifest.yml
 
-bosh deploy mongodb_release/ci_stess_testing/files/manifest.yml \
+bosh deploy mongodb_release/ci_stress_testing/files/manifest.yml \
   --vars-store=/tmp/mongodb_vars.yml \
 
 # Submit example-topology via a Bosh errand
