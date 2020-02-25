d4774a90019636eee4b5b406ff7da794221358e0
Jessica Stenning <jessica.stenning@engineerbetter.com>
2020-02-25 11:08:21 +0000

    Adds blob download and upload to create_release.sh

--- ci_release_pipeline/tasks/create_release.sh
+++ ci_release_pipeline/tasks/create_release.sh
@@ -5,5 +5,11 @@ set -eux
 version="$(cat mongodb_bosh_release_version/version)"
 cd mongodb_bosh_release_git
 
+# Download and add blob
+wget -O /tmp/mongodb-linux-x86_64-ubuntu1604-3.6.6.tgz http://downloads.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-3.6.6.tgz
+bosh add-blob /tmp/mongodb-linux-x86_64-ubuntu1604-3.6.6.tgz mongodb/mongodb-linux-x86_64-3.6.6.tar.gz
+
+# Create candidate release
 bosh create-release --force \
+  --version "$version" \
   --tarball "../release_tarball/mongodb-$version.tgz"
