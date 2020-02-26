5c0822821c595fdb57f70038f29e43c4f6ca50a0
Andy Paine <andy.paine@engineerbetter.com>
2020-02-26 15:15:13 +0000

    Keep errand VMs alive
    Save a bit of time by not creating this VM repeatedly
    Up timeout just in case

--- ci_release_pipeline/tasks/system_test.sh
+++ ci_release_pipeline/tasks/system_test.sh
@@ -16,16 +16,16 @@ bosh upload-release mongodb_bosh_candidate_release_s3/*.tgz
 
 # Deploy and run acceptance test errand
 bosh deploy mongodb_bosh_release_git/ci_release_pipeline/files/manifest.yml
-bosh run-errand acceptance_tests
+bosh run-errand acceptance_tests --keep-alive
 
 # Recreate all vms in the deployment and run acceptance test errand
 bosh recreate
-bosh run-errand acceptance_tests
+bosh run-errand acceptance_tests --keep-alive
 
 # Restart processes on instances and run acceptance test errand
 bosh restart
-bosh run-errand acceptance_tests
+bosh run-errand acceptance_tests --keep-alive
 
 bosh upload-stemcell old_xenial_stemcell/*.tgz
 bosh deploy mongodb_bosh_release_git/ci_release_pipeline/files/manifest.yml -o mongodb_bosh_release_git/ci_release_pipeline/files/use_old_stemcell.yml
-bosh run-errand acceptance_tests
+bosh run-errand acceptance_tests --keep-alive
