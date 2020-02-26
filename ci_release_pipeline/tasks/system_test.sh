ce7cdc91e01eb966e70dbe6dbefbc4042349be11
Andy Paine <andy.paine@engineerbetter.com>
2020-02-26 12:48:16 +0000

    Fix pipeline and system test script
    Using wrong resource and blobstore now needs creds

--- ci_release_pipeline/tasks/system_test.sh
+++ ci_release_pipeline/tasks/system_test.sh
@@ -12,7 +12,7 @@ source /tmp/local-bosh/director/env
 
 # Upload releases and stemcell
 bosh upload-stemcell xenial_stemcell/*.tgz
-bosh upload-release mongodb_release/*.tgz
+bosh upload-release mongodb_bosh_candidate_release_s3/*.tgz
 
 # Deploy and run acceptance test errand
 bosh deploy mongodb_bosh_release_git/ci_release_pipeline/files/manifest.yml
