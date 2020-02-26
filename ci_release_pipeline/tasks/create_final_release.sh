803618506d04302870f6c9c9165875820e92e2cb
Andy Paine <andy.paine@engineerbetter.com>
2020-02-26 12:10:54 +0000

    Build final release tarball from YML file
    The finalized tarball still internally contains the candidate release
    version. Running finalize then create-release we can make a tarball with
    the right version internally.

--- ci_release_pipeline/tasks/create_final_release.sh
+++ ci_release_pipeline/tasks/create_final_release.sh
@@ -3,8 +3,11 @@ set -eux
 
 version="$(cat mongodb_bosh_release_version/version)"
 cd mongodb_bosh_release_git/
+RELEASE_YML=$PWD/releases/mongodb-services/mongodb-services-${version}.yml
 
 echo "creating final release"
 bosh finalize-release ../mongodb_bosh_candidate_release_s3/mongodb-*.tgz \
   --version "$version" \
+
+bosh create-release "${RELEASE_YML}" \
   --tarball "../release_tarball/mongodb-$version.tgz"
